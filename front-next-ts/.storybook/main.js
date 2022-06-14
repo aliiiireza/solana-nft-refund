80f22850cc751a7a7d333eb65a15033f72bf2a65
Alireza Abdollahi <mra.abdollahi@gmail.com>
2022-06-14 17:34:36 +0430

    feat: initialize storybook

---
+++ front-next-ts/.storybook/main.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+module.exports = {
+  stories: [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "../components/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.modules = [path.resolve(__dirname, ".."), "node_modules"];
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@/components": path.resolve(__dirname, "../components"),
+      "@/common": path.resolve(__dirname, "../common"),
+      "@/styles": path.resolve(__dirname, "../styles"),
+    };
+
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+  
+    return config;
+  },
+};
---
+++ front-next-ts/.storybook/preview.js
@@ -0,0 +1,12 @@
+import "antd/dist/antd.dark.css";
+import "../styles/index.scss";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
